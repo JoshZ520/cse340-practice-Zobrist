523d93e0203ab11588b1c7b2b7da6c75178152d1
JoshZ520 <joshuazob@gmail.com>
2025-05-06 10:20:44 -0600

    updates

--- server.js
+++ server.js
@@ -4,7 +4,25 @@ app.set('view engine', 'ejs');
 app.set("views",  path.join(__dirname, 'src/views'));
 // Create an instance of an Express application
 const app = express();
- 
+if (NODE_ENV.includes('dev')) {
+    const ws = await import('ws');
+
+    try {
+        const wsPort = parseInt(PORT) + 1;
+        const wsServer = new ws.WebSocketServer({ port: wsPort });
+
+        wsServer.on('listening', () => {
+            console.log(`WebSocket server is running on port ${wsPort}`);
+    });
+    wsServer.on("listening", () => {
+        console.error('WebSocket server error', error);
+    });
+    
+}
+    catch (error) {
+    console.error('WebSocket server failed to start', error);
+};
+}
 // Define a route handler for the root URL ('/')
 app.get('/', (req, res) => {
     const title = "Home Page";
