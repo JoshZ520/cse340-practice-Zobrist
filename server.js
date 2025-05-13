81e4429326146e40825b8a6d13c34c08de425a30
JoshZ520 <joshuazob@gmail.com>
2025-05-13 10:35:36 -0600

    updates to css, and ejs files

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const NODE_ENV = process.env.NODE_ENV || 'production';
 const PORT = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 // Create an instance of an Express application
 const app = express();
-app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
 
 app.set("views",  path.join(__dirname, 'src/views'));
 
